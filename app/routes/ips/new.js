d5b66d14a0d2d8fbd2d342f5913bc7f4a0bdedbc
Sergey Vyushkov <sergey.unz21@gmail.com>
2016-03-27 14:41:05 +0200

    ehehe

--- app/routes/ips/new.js
+++ app/routes/ips/new.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	activate() {
+		Ember.run.scheduleOnce('afterRender', this, () => {
+			$("#entity-select").select2();
+		});
+	},
 	model() {
 		return this.store.findAll('entity');
 	}
