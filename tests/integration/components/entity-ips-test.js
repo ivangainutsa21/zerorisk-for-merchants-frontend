097bb71e73418cdf273405fdd41db92c0e6ca031
Sergey Vyushkov <sergey.unz21@gmail.com>
2016-04-26 16:03:09 +0200

    fastclick

--- tests/integration/components/entity-ips-test.js
+++ tests/integration/components/entity-ips-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{entity-ips}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#entity-ips}}
-      template block text
-    {{/entity-ips}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Merchant');
 });
