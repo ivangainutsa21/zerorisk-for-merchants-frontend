62fff24a637d5fc034ecb5b89c399c990a46fc05
Sergey Vyushkov <sergey.unz21@gmail.com>
2017-03-17 18:40:28 +0100

    charger la localisation

---
+++ app/instance-initializers/i18n.js
@@ -0,0 +1,25 @@
+export default {
+  name: 'i18n',
+  initialize: function({ container }) {
+    let i18n = container.lookup('service:i18n');
+    
+    i18n.set('locale', calculateLocale(i18n.get('locales')));
+  }
+}
+
+function calculateLocale(locales) {
+  let language = navigator.languages[0] || navigator.language || navigator.userLanguage;
+
+  if(language == "en-GB" || language == "en-US" || language == "en-EN" || language == "en-EN") {
+  	language = "en";
+  }
+  else if(language == "fr-FR" || language == "fr-BE" || language == "fr-CA") {
+  	language = "fr";
+  }
+  else if(language == "nl-NL" || language == "nl-BE") {
+  	language = "nl";
+  }
+
+  return locales.indexOf(language) >= 0 ? language : 'en';
+  //return ocales.includes(language) ? language : 'en';
+}
