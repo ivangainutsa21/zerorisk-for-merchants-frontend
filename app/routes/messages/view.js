68209efb8c8cce94e1d71813b7c83d59263fc5e0
Sergey Vyushkov <sergey.unz21@gmail.com>
2017-03-27 16:02:21 +0200

    attachment download

--- app/routes/messages/view.js
+++ app/routes/messages/view.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import injectService from 'ember-service/inject';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  errorParser: injectService(),
+
   model(params) {
     return this.store.find('message', params.message_id)
+  },
+
+  actions: {
+    downloadAttachment(activeMessage, fileName) {
+      activeMessage.getAttachmentUrl({ fileName: fileName })
+        .then((response) => window.location.assign(response.result.attachments.url))
+        .catch((response) => {
+          this.get('errorParser').parseAndDisplay(response, 'notification');
+        });
+    }
   }
 });
