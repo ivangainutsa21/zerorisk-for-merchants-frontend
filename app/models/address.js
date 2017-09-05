602c27b5c423bcd0775f8e7ac9903fc6e09cd437
Sergey Vyushkov <sergey.unz21@gmail.com>
2017-09-05 11:46:04 +0200

    align address model with pinpoint

--- app/models/address.js
+++ app/models/address.js
@@ -1,34 +1,74 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  businessAddress: [
-    validator('length', { min: 3, max: 30 })
-  ],
-  city: [
-    validator('length', { min: 1, max: 30 })
-  ],
-  stateProvince: [
-    validator('presence', true),
-    validator('length', { min: 1, max: 30 })
-  ],
-  country: [
-    validator('length', { min: 3, max: 30 })
-  ],
-  zipCode: [
-    validator('length', { min: 2, max: 30 })
-  ],
-  url: [
-    validator('format', { type: 'url' }),
-    validator('length', { min: 3, max: 30 })
-  ]
+  tag: {
+    description: 'Tag',
+    validators: [
+      validator('presence', true),
+      validator('length', { min: 1, max: 30 })
+    ]
+  },
+  businessAddress: {
+    description: 'Business Address',
+    validators: [
+      validator('presence', true),
+      validator('length', { min: 1, max: 30 })
+    ]
+  },
+  city: {
+    description: 'City',
+    validators: [
+      validator('presence', true),
+      validator('length', { min: 1, max: 30 })
+    ]
+  },
+
+  stateProvince: {
+    description: "State/Province",
+    validators: [
+      validator('presence', true)
+    ]
+  },
+  country: {
+    description: 'Country',
+    validators: [
+      validator('presence', true),
+      validator('length', { min: 1, max: 30 })
+    ]
+  },
+  zipCode: {
+    description: "ZIP Code",
+    validators: [
+      validator('length', { min: 1, max: 30 })
+    ]
+  },
+  url: {
+    description: "URL",
+    validators: [
+      validator('format', { type: 'url' }),
+      validator('length', { min: 3, max: 30 })
+    ]
+  }
 });
 
 export default DS.Model.extend(Validations, {
+  tag: DS.attr('string'),
+  group: DS.attr('string', { defaultValue: 'World' }),
+  lat: DS.attr('number'),
+  lng: DS.attr('number'),
   businessAddress: DS.attr('string'),
   city: DS.attr('string'),
   stateProvince: DS.attr('string'),
   country: DS.attr('string'),
   zipCode: DS.attr('string'),
-  url: DS.attr('string')
+  url: DS.attr('string'),
+  composedAddress: Ember.computed('businessAddress', function () {
+    if (this.get('businessAddress') && this.get('zipCode')) {
+      return this.get('businessAddress') + ', ' + this.get('zipCode') + ' ' + this.get('city') + ', ' + this.get('country');
+    } else if (this.get('businessAddress')) {
+      return this.get('businessAddress') + ', ' + this.get('city') + ', ' + this.get('country');
+    }
+  })
 });
